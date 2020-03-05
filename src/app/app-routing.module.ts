670b9476673ee7cab753ad953e43c25e4937bc26
Natan Lancha <natanlancha@gmail.com>
2020-03-05 17:42:47 +0100

    Generacion de los routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddpostComponent } from './addpost/addpost.component';
+import { HeaderComponent } from './header/header.component';
+import { MycategoriesComponent } from './mycategories/mycategories.component';
+import { PostComponent } from './post/post.component';
+import { TrenddingComponent } from './trendding/trendding.component';
+import { UserComponent } from './user/user.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'post' },
+  { path: 'addpost', component: AddpostComponent },
+  { path: 'header', component: HeaderComponent },
+  { path: 'mycategories', component: MycategoriesComponent },
+  { path: 'post', component: PostComponent },
+  { path: 'trendding', component: TrenddingComponent },
+  { path: 'user', component: UserComponent },
+  { path: '**', redirectTo: 'post' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
